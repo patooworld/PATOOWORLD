9d13604269ca37f425f9f6f63abd1da66aa7de65
PATOOWORLD <106743064+officialpatoo@users.noreply.github.com>
2024-12-31 20:49:56 -0800

    Update models.ts
    selected providers models currently outperform

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -24,46 +24,4 @@ export const models: Model[] = [
     provider: 'Groq',
     providerId: 'groq'
   },
-  {
-    id: 'qwen2.5',
-    name: 'Qwen 2.5',
-    provider: 'Ollama',
-    providerId: 'ollama'
-  },
-  {
-    id: 'gpt-4o',
-    name: 'GPT-4o',
-    provider: 'OpenAI',
-    providerId: 'openai'
-  },
-  {
-    id: 'gpt-4o-mini',
-    name: 'GPT-4o mini',
-    provider: 'OpenAI',
-    providerId: 'openai'
-  },
-  {
-    id: 'claude-3-5-sonnet-latest',
-    name: 'Claude 3.5 Sonnet',
-    provider: 'Anthropic',
-    providerId: 'anthropic'
-  },
-  {
-    id: 'claude-3-5-haiku-20241022',
-    name: 'Claude 3.5 Haiku',
-    provider: 'Anthropic',
-    providerId: 'anthropic'
-  },
-  {
-    id: process.env.NEXT_PUBLIC_AZURE_DEPLOYMENT_NAME || 'undefined',
-    name: process.env.NEXT_PUBLIC_AZURE_DEPLOYMENT_NAME || 'Undefined',
-    provider: 'Azure',
-    providerId: 'azure'
-  },
-  {
-    id: process.env.NEXT_PUBLIC_OPENAI_COMPATIBLE_MODEL || 'undefined',
-    name: process.env.NEXT_PUBLIC_OPENAI_COMPATIBLE_MODEL || 'Undefined',
-    provider: 'OpenAI Compatible',
-    providerId: 'openai-compatible'
-  }
 ]
