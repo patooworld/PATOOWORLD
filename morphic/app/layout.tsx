62ff211c22f30fc67ded1de38341589e78fff498
PATOOWORLD <106743064+officialpatoo@users.noreply.github.com>
2024-12-31 18:38:24 -0800

    Update layout.tsx

--- morphic/app/layout.tsx
+++ morphic/app/layout.tsx
@@ -14,12 +14,12 @@ const fontSans = FontSans({
   variable: '--font-sans'
 })
 
-const title = 'Morphic'
+const title = 'PATOOWORLD'
 const description =
-  'A fully open-source AI-powered answer engine with a generative UI.'
+  'AI-powered Search Based on Choice.'
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://morphic.sh'),
+  metadataBase: new URL('https://patooworld.com'),
   title,
   description,
   openGraph: {
