c7b73b6f9cc463f3e2b81ee4706d800d622233ff
PATOOWORLD <106743064+officialpatoo@users.noreply.github.com>
2025-01-05 14:01:38 -0800

    Update models.ts
    testing other model

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -18,4 +18,28 @@ export const models: Model[] = [
     provider: 'Groq',
     providerId: 'groq'
   },
+ {
+    id: 'gpt-4o',
+    name: 'GPT-4o',
+    provider: 'OpenAI',
+    providerId: 'openai'
+  },
+  {
+    id: 'gpt-4o-mini',
+    name: 'GPT-4o mini',
+    provider: 'OpenAI',
+    providerId: 'openai'
+  },
+  {
+    id: 'claude-3-5-sonnet-latest',
+    name: 'Claude 3.5 Sonnet',
+    provider: 'Anthropic',
+    providerId: 'anthropic'
+  },
+  {
+    id: 'claude-3-5-haiku-20241022',
+    name: 'Claude 3.5 Haiku',
+    provider: 'Anthropic',
+    providerId: 'anthropic'
+  },
 ]
