590403ddcabc2bf91f92bf57c6c288907da97999
PATOOWORLD <106743064+officialpatoo@users.noreply.github.com>
2025-01-05 03:47:45 -0500

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,7 @@ export default function RootLayout({
           <AppStateProvider>
             <Header />
             {children}
+            <Sidebar />
             <Footer />
             <Toaster />
           </AppStateProvider>
