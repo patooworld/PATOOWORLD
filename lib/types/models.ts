38b2a3d6f4a54dfbad9b4620fa3e3650fcd2bd78
PATOOWORLD <106743064+officialpatoo@users.noreply.github.com>
2025-01-05 03:12:16 -0500

    Update models.ts
    Updated to name that resonates the default with suggestions

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -12,4 +12,10 @@ export const models: Model[] = [
     provider: 'Google Generative AI',
     providerId: 'google'
   },
+   {
+    id: 'llama3-groq-8b-8192-tool-use-preview',
+    name: 'Grok 99pace',
+    provider: 'Groq',
+    providerId: 'groq'
+  },
 ]
