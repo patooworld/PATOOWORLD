f13a79d5f4ced24a78e17f5b1c2884ae7e1e13d3
PATOOWORLD <106743064+officialpatoo@users.noreply.github.com>
2025-01-06 16:04:18 -0800

    Update models.ts
    add azure experiment

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -18,4 +18,10 @@ export const models: Model[] = [
     provider: ',',
     providerId: 'groq'
   },
+  {
+    id: process.env.NEXT_PUBLIC_AZURE_DEPLOYMENT_NAME || 'undefined',
+    name: process.env.NEXT_PUBLIC_AZURE_DEPLOYMENT_NAME || 'Undefined',
+    provider: 'Azure',
+    providerId: 'azure'
+  },
 ]
