75fe8e8f87465ec725490ab0251e8aed28aa7744
PATOOWORLD <support@patooworld.com>
2025-01-07 19:17:50 -0800

    Update empty-screen.tsx
    updated to ideal

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -7,16 +7,16 @@ const exampleMessages = [
     message: 'gemini is a large language model created by google'
   },
   {
-    heading: 'Latest Technology Trends',
-    message: 'Latest Technology Trends'
+    heading: 'Why is Nvidia growing rapidly?',
+    message: 'Why is Nvidia growing rapidly?'
+  },
+  {
+    heading: 'Tesla vs Rivian',
+    message: 'Tesla vs Rivian'
   },
   {
     heading: 'Latest Technology Trends',
     message: 'Latest Technology Trends'
-  },
-  {
-    heading: 'Summary: https://arxiv.org/pdf/2407.16833',
-    message: 'Summary: https://arxiv.org/pdf/2407.16833'
   }
 ]
 export function EmptyScreen({
