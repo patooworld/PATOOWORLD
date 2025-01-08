85d3632cad03c41bd352f1180fb7ac7a21ec643d
PATOOWORLD <support@patooworld.com>
2025-01-07 22:09:54 -0800

    models.ts

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -9,13 +9,13 @@ export const models: Model[] = [
   {
     id: 'gemini-2.0-flash-exp',
     name: 'PATOOWORLD',
-    provider: 'Gemini Flash',
+    provider: '',
     providerId: 'google'
   },
 {
   id: 'gemini-1.5-pro-002',
   name: '99PACE',
-  provider: 'Gemini pro',
+  provider: '',
   providerId: 'google'
   },
 ]
