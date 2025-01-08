020d0569766289e5864357dc9d4f0b3d85ef73d5
PATOOWORLD <support@patooworld.com>
2025-01-07 17:44:34 -0800

    Update empty-screen.tsx
    changed default to ideal

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -3,16 +3,16 @@ import { ArrowRight } from 'lucide-react'
 
 const exampleMessages = [
   {
-    heading: 'What is OpenAI o1?',
-    message: 'What is OpenAI o1?'
+    heading: 'gemini is a large language model created by google',
+    message: 'gemini is a large language model created by google'
   },
   {
-    heading: 'Why is Nvidia growing rapidly?',
-    message: 'Why is Nvidia growing rapidly?'
+    heading: 'Latest Technology Trends',
+    message: 'Latest Technology Trends'
   },
   {
-    heading: 'Tesla vs Rivian',
-    message: 'Tesla vs Rivian'
+    heading: 'Latest Technology Trends',
+    message: 'Latest Technology Trends'
   },
   {
     heading: 'Summary: https://arxiv.org/pdf/2407.16833',
