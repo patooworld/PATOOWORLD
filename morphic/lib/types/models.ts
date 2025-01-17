3fdad3827a9fc8365978d5078b66bf84c1012f58
Elevationpace <support@patooworld.com>
2025-01-17 04:16:08 -0800

    Update models.ts

--- morphic/lib/types/models.ts
+++ morphic/lib/types/models.ts
@@ -8,7 +8,7 @@ export interface Model {
 export const models: Model[] = [
   {
     id: 'gemini-1.5-pro-002',
-    name: 'PATOOWORLD',
+    name: 'gemini 1.5 pro',
     provider: 'Google Generative AI',
     providerId: 'google'
   };
